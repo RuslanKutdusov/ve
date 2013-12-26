bd93eeac0c45f105d670ed72f4a4f0b4fdaa87af
Ruslan.Kutdusov <ruslan.kutdusov.usu@gmail.com>
2013-12-26 23:40:16 +0600

    fix some stupid

--- lab1.cpp
+++ lab1.cpp
@@ -4,34 +4,34 @@
 
 
 // ф-ия из первой части лабы
-double f1( double x )
+long double f1( long double x )
 {
 	return sqrtf( 1.0f + 11.0f * cosf( x ) );
 }
 
 
 // из второй части лабы
-double f2( double x )
+long double f2( long double x )
 {
 	return 1.0f / ( 1.0f - x + x * x );
 }
 
 
 //
-typedef std::function< double( double ) > Func;
-typedef std::function< double( Func, int, double, double ) > Method;
+typedef std::function< long double( long double ) > Func;
+typedef std::function< long double( Func, int, long double, long double ) > Method;
 
 
 // метод ср. прямоугольников
-double AvgSquare( Func func, int n, double a, double b )
+long double AvgSquare( Func func, int n, long double a, long double b )
 {
-	double h = ( b - a ) / ( double )n;
+	long double h = ( b - a ) / ( long double )n;
 
-	double sum = 0.0f;
+	long double sum = 0.0f;
 
 	for( int i = 0; i < n; i++ )
 	{
-		double x = ( double )i * h;
+		long double x = ( long double )i * h;
 		sum += func( x - h / 2.0f );
 	}
 
@@ -40,15 +40,15 @@ double AvgSquare( Func func, int n, double a, double b )
 
 
 // трапеций
-double Trapeze( Func func, int n, double a, double b )
+long double Trapeze( Func func, int n, long double a, long double b )
 {
-	double h = ( b - a ) / ( double )n;
+	long double h = ( b - a ) / ( long double )n;
 
-	double sum = 0.0f;
+	long double sum = 0.0f;
 
 	for( int i = 1; i < n; i++ )
 	{
-		double xi = a + ( double )i * h;
+		long double xi = a + ( long double )i * h;
 
 		sum += func( xi );
 	}
@@ -61,14 +61,14 @@ double Trapeze( Func func, int n, double a, double b )
 
 
 // симпосна
-double Simpson( Func func, int n, double a, double b )
+long double Simpson( Func func, int n, long double a, long double b )
 {
-	double h = ( b - a ) / ( double )n;
+	long double h = ( b - a ) / ( long double )n;
 
-	double sum = 0.0f;
-	for( int i = 0; i < n; i += 2 )
+	long double sum = 0.0f;
+	for( int i = 1; i < n; i += 2 )
 	{
-		double xi = ( double )i * h;
+		long double xi = ( long double )i * h;
 		sum += func( xi - h ) + 4.0f * func( xi ) + func( xi + h );
 	}
 
@@ -79,14 +79,14 @@ double Simpson( Func func, int n, double a, double b )
 // 
 void ApplyMethod( Method method, int k )
 {
-	double h1 	= 		 method( f1, 10, 0.0f, 1.0f );
-	double h2 	= 		 method( f1, 20, 0.0f, 1.0f );
+	long double h1 	= 		 method( f1, 10, 0.0f, 1.0f );
+	long double h2 	= 		 method( f1, 20, 0.0f, 1.0f );
 
 	// правило Рунге
-	double Rn = fabs( h2 - h1 ) / ( powf( 2.0f, ( double )k ) - 1.0f );
+	long double Rn = fabs( h2 - h1 ) / ( powf( 2.0f, ( long double )k ) - 1.0f );
 
-	printf( "%f %f\n", h1, Rn );
-	printf( "%f\n", h2 );	
+	printf( "%Lg %Lg\n", h1, Rn );
+	printf( "%Lg\n", h2 );	
 }
 
 
@@ -101,13 +101,13 @@ int main()
 	// вторая
 	FILE* file = fopen( "data", "w" );
 
-	double f2Val = 2.0f * 3.1415926f / 3.0f / sqrt( 3.0f );
+	long double f2Val = 2.0f * 3.1415926f / 3.0f / sqrt( 3.0f );
 	for( int n = 2; n < 50; n += 2 )
 	{
-		double gAvgSq 		= fabs( AvgSquare( f2, n, 0.0f, 1.0f ) - f2Val);
-		double gTrapeze 	= fabs( Trapeze( f2, n, 0.0f, 1.0f ) - f2Val );
-		double gSimpson 	= fabs( Simpson( f2, n, 0.0f, 1.0f ) - f2Val );
-		fprintf( file, "%d %.8f %.8f %.8f\n", n, gAvgSq, gTrapeze, gSimpson );
+		long double gAvgSq 		= fabs( AvgSquare( f2, n, 0.0f, 1.0f ) - f2Val);
+		long double gTrapeze 	= fabs( Trapeze( f2, n, 0.0f, 1.0f ) - f2Val );
+		long double gSimpson 	= fabs( Simpson( f2, n, 0.0f, 1.0f ) - f2Val );
+		fprintf( file, "%d %Lg %Lg %Lg\n", n, gAvgSq, gTrapeze, gSimpson );
 	}
 	fclose( file );
 
